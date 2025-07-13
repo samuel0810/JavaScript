1578ee608c1d788f52fc83c41a633016adf45c0c
samuel0810 <samueljimenez080708@gmail.com>
2025-07-13 13:08:17 -0400

    a

---
+++ ejercicio-logica.js
@@ -0,0 +1,37 @@
+/*
+let edad = 20
+let tienePermiso = true
+
+if (edad >= 18 && tienePermiso){
+    console.log("Tienen permiso")
+}
+
+else{
+    console.log("No tiene permiso")
+}
+
+
+
+let generoPelicula = "accion"
+let clasificacionPelicula = "PG-13"
+
+if (generoPelicula === "ciencia ficcion" || clasificacionPelicula === "R"){
+    console.log("Coincide con las reglas")
+}
+
+else{
+    console.log("No coincide con las reglas")
+}
+
+
+
+let estaActivo = true
+
+if (!estaActivo){
+    console.log("Usuario inactivo")
+}
+
+else{
+    console.log("Usuario activo")
+}
+*/
