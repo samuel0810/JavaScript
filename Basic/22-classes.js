9ae4fb85ec69417c05e4f362c2f1be4b1450b01b
samuel0810 <samueljimenez080708@gmail.com>
2025-07-28 15:31:23 -0400

    classes

--- Basic/22-classes.js
+++ Basic/22-classes.js
@@ -1,2 +1,65 @@
 // Clases
 
+class Person {
+
+    constructor(name, age, alias) { 
+        this.name = name
+        this.age = age
+        this.alias = alias
+    }
+
+}
+
+
+// Sintaxis
+
+let person = new Person("Samuel", 17, "Jimenez")
+let person2 = new Person("Samuel", 17, "Jimenez")
+
+console.log(person)
+console.log(person2)
+
+console.log(typeof person)
+
+// Valores por defecto
+
+class DefaultPerson {
+
+    constructor(name = "Sin nombre", age = 0, alias = "Sin alias") { 
+        this.name = name
+        this.age = age
+        this.alias = alias
+    }
+
+}
+
+let person3 = new DefaultPerson("Samue;", 17)
+console.log(person3)
+
+// Acceso a propiedades
+
+console.log(person3.alias)
+console.log(person3["alias"])
+
+person3.alias = "Jimenez"
+
+console.log(person3.alias)
+
+// Funciones a clases
+
+class PersonWithMethod {
+
+    constructor(name, age, alias) { 
+        this.name = name
+        this.age = age
+        this.alias = alias
+    }
+
+    walk() {
+        console.log("La persona camina")
+    }
+
+}
+
+let person4 = new PersonWithMethod("Samuel", 17, "Jimenez")
+person4.walk()
