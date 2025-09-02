9f4fb149b0153c62d5d537371b8b7bf8a900a375
samuel0810 <samueljimenez080708@gmail.com>
2025-09-01 20:49:15 -0400

    cambios

---
+++ Platzi/find-findIndex.js
@@ -0,0 +1,15 @@
+// find()
+
+const multipleOf5 = [5, 10, 15, 20]
+const firstNumberGreaterThan10 = multipleOf5.find(number => number > 10)
+
+console.log(multipleOf5)
+console.log(firstNumberGreaterThan10)
+
+// findIndex()
+
+const randomNumbers = [6, 14, 27, 56, 40]
+const indexNumber = randomNumbers.findIndex(number => number > 50)
+
+console.log(randomNumbers)
+console.log(indexNumber)
